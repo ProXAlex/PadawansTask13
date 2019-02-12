3811606656c99d395007f0c9777dca6c584c74cc
Alex <alex_goreloshev@mail.ru>
2019-02-12 18:00:16 +0300

    Add Employee class

---
+++ Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace PadawansTask13
+{
+    public class Employee
+    {
+        public Employee()
+        {
+        }
+
+        public Employee(string surname, int age)
+        {
+            Surname = surname;
+            Age = age;
+        }
+
+        private string Surname { get; set; }
+
+        private int Age { get; set; }
+
+        private string GetAgeAsString() => Age.ToString();
+
+        public void ChangeSurname(string newSurname) => Surname = newSurname;
+
+        public string GetInfo() => $"Surname: {Surname}, Age: {Age}";
+    }
+}
